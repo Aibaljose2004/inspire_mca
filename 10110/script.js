01279f395a5dab11ca092a1320c6ab6312b4cf42
Aibal Jose <117338506+Aibaljose2004@users.noreply.github.com>
2023-04-22 21:45:35 +0530

    Update script.js

--- 10110/script.js
+++ 10110/script.js
@@ -6,11 +6,7 @@ clue.style.display = "none";
 
 
 
-function nospaces(t) {
-    if (t.value.match(/\s/g)) {
-        t.value = t.value.replace(/\s/g, '');
-    }
-}
+
 
 
 btn.onclick = function () {
