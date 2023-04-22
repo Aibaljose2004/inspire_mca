859c4532c0b5ab3742865e86a750d127e34a2aac
Aibal Jose <117338506+Aibaljose2004@users.noreply.github.com>
2023-04-22 21:50:01 +0530

    Update script.js

--- 10110/script.js
+++ 10110/script.js
@@ -6,10 +6,14 @@ clue.style.display = "none";
 
 
 
+function nospaces(t) {
+    if (t.value.match(/\s/g)) {
+        t.value = t.value.replace(/\s/g, '');
+    }
+}
 
-
-
-btn.onclick = function () {
+ 
+btn.addEventListener('click', function (){
     console.log(value.value)
     if (value.value == "dennisritchie") {
         box.style.display = "none";
