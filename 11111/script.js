f96f23e46401c297ee2ae8c310c6ac0a8951fc44
Aibal Jose <117338506+Aibaljose2004@users.noreply.github.com>
2023-04-22 23:12:03 +0530

    Update script.js

--- 11111/script.js
+++ 11111/script.js
@@ -7,7 +7,7 @@ const clue = document.getElementById("clue");
 
 
 
-clue.style.display = "none";
+
 function nospaces(t) {
     if (t.value.match(/\s/g)) {
         t.value = t.value.replace(/\s/g, '');
