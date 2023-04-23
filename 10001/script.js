4ba27e26a907f93e66cdafaf2d089274b98a9b37
Aibal Jose <117338506+Aibaljose2004@users.noreply.github.com>
2023-04-23 10:47:31 +0530

    Update script.js

--- 10001/script.js
+++ 10001/script.js
@@ -7,7 +7,7 @@ const clue = document.getElementById("clue");
 
 
 
-clue.style.display = "none";
+
 function nospaces(t) {
     if (t.value.match(/\s/g)) {
         t.value = t.value.replace(/\s/g, '');
@@ -17,7 +17,6 @@ function nospaces(t) {
 
 
 
-clue.style.display = "none";
 btn.addEventListener("click", function() {
     
     if (value.value == "niger") {
